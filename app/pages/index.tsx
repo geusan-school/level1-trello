305688cba48a3c969e436d582de20698df6c079a
geusan <dnay2k@gmail.com>
2022-10-01 13:20:51 +0900

    refac: trello context

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,17 +1,88 @@
-import { Box, Button, Heading, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Stack, Text, useDisclosure } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import type { NextPage } from 'next'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import Section from '../src/components/Section'
 
+export type TaskType = {
+  id: number;
+  title: string;
+  description: string;
+}
+type ContextProps = {
+  tasks: TaskType[][],
+  addTask(sectionIndex: number, newTask: TaskType): void,
+  deleteTask(sectionIndex: number, newTask: TaskType): void,
+  moveTask(beforeSectionIndex: number, afterSectionIndex: number, newTask: TaskType): void,
+  swapTask(sectionIndex: number, task: TaskType, movement: number): void,
+}
+export const TrelloContext = React.createContext<ContextProps>({
+  tasks: [],
+} as any);
+
 const Home: NextPage = () => {
+  const [tasks, setTasks] = useState<TaskType[][]>([
+    [],
+    [],
+    [],
+  ]);
+  const addTask = (sectionIndex: number, newTask: TaskType) => {
+    setTasks(prev => {
+      return [
+        ...prev.slice(0, sectionIndex),
+        [newTask, ...prev[sectionIndex]],
+        ...prev.slice(sectionIndex + 1),
+      ]
+    });
+  }
+  const deleteTask = (sectionIndex: number, task: TaskType) => {
+    setTasks(prev => {
+      const idx = prev[sectionIndex].findIndex(t => t.id === task.id);
+      return [
+        ...prev.slice(0, sectionIndex),
+        [
+          ...prev[sectionIndex].slice(0, idx),
+          ...prev[sectionIndex].slice(idx + 1),
+        ],
+        ...prev.slice(sectionIndex + 1),
+      ]
+    });
+  }
+  const moveTask = (beforeSectionIndex: number, afterSectionIndex: number, task: TaskType) => {
+    deleteTask(beforeSectionIndex, task);
+    addTask(afterSectionIndex, task);
+  }
+  const swapTask = (sectionIndex: number, task:TaskType, movement: 1 | -1) => {
+    setTasks(prev => {
+      const idx = prev[sectionIndex].findIndex(t => t.id === task.id);
+      const anotherIndex = idx + movement;
+      return [
+        ...prev.slice(0, sectionIndex),
+        [
+          ...prev[sectionIndex].slice(0, Math.min(idx, anotherIndex)),
+          ...(movement > 0 ? [
+            prev[sectionIndex][anotherIndex],
+            prev[sectionIndex][idx],
+          ] : [
+            prev[sectionIndex][idx],
+            prev[sectionIndex][anotherIndex],
+          ]),
+          ...prev[sectionIndex].slice(Math.max(idx, anotherIndex) + 1),
+        ],
+        ...prev.slice(sectionIndex + 1),
+      ]
+    })
+  };
+
   return (
-    <Box display="flex" justifyContent="space-between" p={4}>
-      <Section title="To Do" />
-      <Box w={4} />
-      <Section title="Doing" />
-      <Box w={4} />
-      <Section title="Done" />
-    </Box>
+    <TrelloContext.Provider value={{ tasks, addTask, deleteTask, moveTask, swapTask }}>
+      <Box display="flex" justifyContent="space-between" p={4}>
+        <Section title="To Do" index={0} />
+        <Box w={4} />
+        <Section title="Doing" index={1} />
+        <Box w={4} />
+        <Section title="Done" index={2} />
+      </Box>
+    </TrelloContext.Provider>
   )
 }
 
--- app/src/components/Section.tsx
+++ app/src/components/Section.tsx
@@ -1,25 +1,90 @@
 import { Box, Button, Heading, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Stack, Text, useDisclosure } from "@chakra-ui/react";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { TaskType, TrelloContext } from "../../pages";
 
-type TaskType = {
-  id: number;
-  title: string;
-  description: string;
-}
+const DATA = [
+  {
+    id: 1,
+    title: "1",
+    description: "content1"
+  },
+  {
+    id: 2,
+    title: "2",
+    description: "content2"
+  },
+  {
+    id: 3,
+    title: "3",
+    description: "content3"
+  },
+  {
+    id: 4,
+    title: "4",
+    description: "content4"
+  },
+  {
+    id: 5,
+    title: "5",
+    description: "content5"
+  },
+]
 
-export default function Section({ title: sectionTitle }: { title: string }) {
+export default function Section({ title: sectionTitle, index }: { title: string, index: number }) {
   const { isOpen, onClose, onOpen } = useDisclosure();
   const { isOpen: updateModalIsOpen, onClose: onUpdateModalClose, onOpen: onUpdateModalOpen } = useDisclosure();
   const [currentId, setCurrentId] = useState(-1);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [tasks, setTasks] = useState<TaskType[]>([]);
+  const [, setTasks] = useState<TaskType[]>(DATA);
+  const openCreateModal = () => onOpen();
+  const { addTask: handleAddTask, tasks: sections, moveTask, deleteTask, swapTask } = useContext(TrelloContext);
+  const tasks = sections[index];
+  const addTask = () => {
+    // 카드 추가
+    handleAddTask?.(index, {
+      id: Date.now(),
+      title: title,
+      description,
+    });
+    // 입력값 초기화
+    setTitle('');
+    setDescription('');
+    // 모달 닫기
+    onClose();
+  }
+  const openUpdateModal = (task: TaskType) => {
+    onUpdateModalOpen();
+    setTitle(task.title);
+    setDescription(task.description);
+    setCurrentId(task.id);
+  }
+  const updateTask = () => {
+    const idx = tasks.findIndex(t => t.id === currentId);
+    setTasks([
+      ...tasks.slice(0, idx),
+      {
+        id: currentId,
+        title,
+        description,
+      },
+      ...tasks.slice(idx + 1),
+    ]);
+    onUpdateModalClose();
+  }
+  const moveUpTask = (task: TaskType) => {
+    swapTask(index, task, -1);
+  }
+  const moveDownTask = (task: TaskType) => {
+    swapTask(index, task, 1);
+  }
+  const moveRightTask = (task: TaskType) => {
+    moveTask(index, index + 1, task);
+  }
   return (
     <Box bg="tomato" flex={1} borderRadius={4} p={4}>
       <Heading>{sectionTitle}</Heading>
-      <Button onClick={() => {
-        onOpen();
-      }}>
+      <Button onClick={openCreateModal}>
         할일 추가
       </Button>
       <Stack>
@@ -27,20 +92,18 @@ export default function Section({ title: sectionTitle }: { title: string }) {
           <Box key={task.id} bg="cyan.200" p={4} borderRadius={4}>
             <Heading fontSize="md">{task.title}</Heading>
             <Text>{task.description}</Text>
-            <Button onClick={() => {
-              const idx = tasks.findIndex(t => t.id === task.id);
-              setTasks([
-                ...tasks.slice(0, idx),
-                ...tasks.slice(idx + 1),
-              ])
-            }}>삭제</Button>
-            <Button onClick={() => {
-              onUpdateModalOpen();
-              setTitle(task.title);
-              setDescription(task.description);
-              setCurrentId(task.id);
-            }}>
+            <Button onClick={() => deleteTask(index, task)}>삭제</Button>
+            <Button onClick={() => openUpdateModal(task)}>
               수정
+            </Button>
+            <Button onClick={() => moveUpTask(task)}>
+              위로
+            </Button>
+            <Button onClick={() => moveDownTask(task)}>
+              아래로
+            </Button>
+            <Button onClick={() => moveRightTask(task)}>
+              오른쪽으로
             </Button>
           </Box>
         ))}
@@ -53,22 +116,7 @@ export default function Section({ title: sectionTitle }: { title: string }) {
             <Input placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} />
           </ModalBody>
           <ModalFooter>
-            <Button onClick={() => {
-              // 카드 추가
-              setTasks([
-                ...tasks,
-                {
-                  id: Date.now(),
-                  title: title,
-                  description,
-                }
-              ]);
-              // 입력값 초기화
-              setTitle('');
-              setDescription('');
-              // 모달 닫기
-              onClose();
-            }}>추가하기</Button>
+            <Button onClick={addTask}>추가하기</Button>
             <Button variant="text" onClick={onClose}>닫기</Button>
           </ModalFooter>
         </ModalContent>
@@ -81,19 +129,7 @@ export default function Section({ title: sectionTitle }: { title: string }) {
             <Input placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} />
           </ModalBody>
           <ModalFooter>
-            <Button onClick={() => {
-              const idx = tasks.findIndex(t => t.id === currentId);
-              setTasks([
-                ...tasks.slice(0, idx),
-                {
-                  id: currentId,
-                  title,
-                  description,
-                },
-                ...tasks.slice(idx + 1),
-              ]);
-              onUpdateModalClose();
-            }}>수정하기</Button>
+            <Button onClick={updateTask}>수정하기</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
